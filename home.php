0ac434d4f3a3e07a03371e371fa29ca81da91f2c
ledutu <javajava159@gmail.com>
2019-11-03 13:17:48 +0700

    finish home with css

--- home.php
+++ home.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Youtube</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -13,35 +15,111 @@
     
 </head>
 <body>
-    <div class="header">
-        <nav class="navbar navbar-default" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-            <a href="#" class="btn btn-lg">
-                <span class="glyphicon glyphicon-menu-hamburger"></span>
-            </a>
-            </div>
-        
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link</a></li>
-                    <li><a href="#">Link</a></li>
-                </ul>
-                <form class="navbar-form navbar-left" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
+    <div class="application">
 
-                <!-- right -->
-                <ul class="nav navbar-nav navbar-right">
-                    
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
+        <div class="header"> 
+            <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a href="#" class="btn btn-lg navbar-brand">
+                        <i class="fa fa-youtube glyphicon fa-lg"></i>
+                    </a>
+                </div>
+            
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav title">
+                        <li><a href="#">Youtube</a></li>
+                    </ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <a href="#" class="btn" type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </a>
+                    </form>
+
+                    <!-- right -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-facetime-video"></span> Video up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+        </div>  <!-- finish header menu -->
         
+
+
+        <div class="contentWrapper" style="margin-top: 70px">
+            <div class="container-fluid">
+
+                <div class="row">
+
+                    <!-- left menu -->
+                    <div class="col-lg-2 leftMenu">
+                        <a href="" style="display: block" ><i class="fa fa-home fa-lg"></i> Home</a> 
+                        <a href="" style="display: block" ><i class="fa fa-info fa-lg"></i> About</a> 
+                        <a href="" style="display: block" ><i class="fa fa-fire fa-lg"></i> Service</a> 
+                        <a href="" style="display: block" ><i class="fa fa-address-book fa-lg"></i> Contact</a> 
+                        
+                    
+                    </div>  <!-- finish left menu -->
+                    
+                    <!-- content -->
+                    <div class="col-lg-10 content">
+                        <div class="container">
+
+                            <!-- create item wrapper -->
+                            <?php
+                            for($i=0; $i < 3; $i++)
+                            {
+                            ?>
+                            <div class="row">
+                                
+                                <div class="row sectionRow">
+                                    
+                                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                                        <h2>Đề xuất <?php echo $i ?></h2>
+                                    </div>
+
+                                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 text-right">
+                                        <h2><button class="btn btn-default">Read more</button></h2>
+                                    </div>
+
+                                </div>
+
+                                <!-- to create 8 items -->
+                                <?php
+                                for($j=0; $j<8; $j++)
+                                {
+                                ?>
+                                <div class="col-xs-12 col-sm-6 col-md-3">
+                                    <div class="item">
+                                        <img src="https://picsum.photos/id/<?php echo $i*10+$j+10 ?>/600/600" alt="Image" class="img-responsive">
+                                        <h3><a href="">Tran danh hay nhat lich su</a></h3>
+                                        <p>Nguoi dang</p>
+                                        <p style="float: left">160N Luot xem - </p>
+                                        <p>1 nam truoc</p>
+                                    </div>
+                                </div>
+                                <?php
+                                }
+                                ?>
+                                
+                            </div>  <!-- End row -->
+                            <?php
+                            }
+                            ?>
+                        </div>  <!-- End container -->
+                    </div>  <!-- End content -->
+                </div>
+
+            </div>
+        </div>
+
+
     </div>
     
 </body>
